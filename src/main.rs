506d65e3b07925f64ee1f1a5cd48d385cdb33500
Collin Watson <collinwat@users.noreply.github.com>
2026-02-08 09:11:26 -0800

    Initial Bevy project scaffold

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "hexorder".to_string(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
